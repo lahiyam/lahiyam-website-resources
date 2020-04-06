b61fc4d3fa01851d4dfde526f80e39c48f97a7c0
lahiyam <londonchaim@gmail.com>
2020-04-06 12:21:04 -0700

    Made deploy.sh script more modular

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 npm install -g serverless
 serverless deploy --stage $env --package \
-$CODEBUILD_SRC_DIR/target/$env -v -r us-east-1
+$CODEBUILD_SRC_DIR/services/$service/target/$env -v -r $AWS_REGION
