f0fdfe7c339ae716a750291a2e398e5b1489d17a
lahiyam <londonchaim@gmail.com>
2020-04-06 15:49:24 -0700

    updated resource names

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 npm install -g serverless
 cd $CODEBUILD_SRC_DIR/services/$service || exit
-ls
 serverless deploy --stage $env --package \
 $CODEBUILD_SRC_DIR/services/$service/target/$env -v -r $AWS_REGION
